d802352cf5800d32fe2ebabe5fedb06f15886da4
husseinahmed <hussenahmedkhalif122@gmail.com>
2024-05-26 20:03:40 +0300

    dom-modified lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+newHeader.innerText = 'Hussein is the champion';
+
+document.body.appendChild(newHeader);
+
+
+
+ 
+  
